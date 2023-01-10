9d4b06123dc3fd2755e0c1912878cc41c3e8253a
jeonbyeongmin <qudals7613@gmail.com>
2023-01-10 22:35:50 +0900

    feat: Component를 상속받는 Page를 만든다

---
+++ src/components/core/Page.js
@@ -0,0 +1,12 @@
+import Component from '@components/core/Component';
+
+export default class Page extends Component {
+  $params;
+  constructor($target, $props, $params) {
+    super($target, $props);
+    this.$params = $params;
+  }
+  setTitle(title) {
+    document.title = title;
+  }
+}
