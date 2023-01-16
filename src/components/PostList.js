a6c90f3044574ebfdae156331b196fc4a00f14de
jeonbyeongmin <qudals7613@gmail.com>
2023-01-16 18:25:57 +0900

    design: post list에 대한 스켈레톤 추가

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,6 +3,32 @@ import Post from '@components/Post';
 
 export default class PostList extends Component {
   template() {
+    if (this.props.posts.length === 0) {
+      const skeleton = new Array(15).fill(0);
+      const $skeleton = skeleton
+        .map(() => {
+          return `
+          <div class="animate-pulse cursor-pointer flex w-full flex-row gap-3 mb-3">
+            <div class="rounded-lg bg-slate-100 object-cover h-24 w-24"></div>
+            <div class="flex-1 space-y-5 py-1">
+              <div class="h-2 bg-slate-100 w-40 rounded"></div>
+              <div class="h-2 bg-slate-100 rounded"></div>
+              <div class="space-y-3">
+                <div class="grid grid-cols-3 gap-4">
+                  <div class="h-2 bg-slate-100 rounded col-span-2"></div>
+                  <div class="h-2 bg-slate-100 rounded col-span-1"></div>
+                </div>
+                <div class="h-2 bg-slate-100 rounded"></div>
+              </div>
+            </div>
+          </div>
+        `;
+        })
+        .join('');
+
+      return $skeleton;
+    }
+
     return `
       <div class='w-full mb-5 card-list flex flex-col gap-3' data-component="post-list"></div>
     `;
